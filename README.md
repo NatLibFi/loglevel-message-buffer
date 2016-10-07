59cfd8893a0fd40ea5629775195ca3461f7b1b41
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2016-10-07 09:41:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# loglevel Message buffer plugin [![NPM Version](https://img.shields.io/npm/v/loglevel-message-buffer.svg)](https://npmjs.org/package/loglevel-message-buffer) [![Build Status](https://travis-ci.org/NatLibFi/loglevel-message-buffer.svg)](https://travis-ci.org/NatLibFi/loglevel-message-buffer) [![Test Coverage](https://codeclimate.com/github/NatLibFi/loglevel-message-buffer/badges/coverage.svg)](https://codeclimate.com/github/NatLibFi/loglevel-message-buffer/coverage)
+
+Plugin for [loglevel](https://github.com/pimterry/loglevel) which allows buffering of log messages
+
+## Usage
+
+#### AMD
+
+```javascript
+
+define(['loglevel', 'loglevel-message-buffer'], function(log, loglevelMessageBuffer) {
+
+  loglevelMessageBuffer(log);
+
+  log.warn('TEST');
+  // Flushes the buffer and sends all messages forward
+  log.flush();
+
+});
+
+```
+
+#### Node.js require
+
+```javascript
+
+var log = require('loglevel');
+var loglevelMessageBuffer = require('loglevel-message-buffer');
+
+loglevelMessageBuffer(log);
+
+log.warn('TEST');
+  // Flushes the buffer and sends all messages forward
+log.flush();
+
+```
+
+## Development 
+
+Clone the sources and install the package using `npm`:
+
+```sh
+npm install
+```
+
+Run the following NPM script to lint, test and check coverage of the code:
+
+```javascript
+
+npm run check
+
+```
+
+## License and copyright
+
+Copyright (c) 2016 **University Of Helsinki (The National Library Of Finland)**
+
+This project's source code is licensed under the terms of **MIT License**.
---
+++ test/main.spec.js
@@ -0,0 +1,183 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * Plugin for loglevel which allows buffering of log messages
+ *
+ * Copyright (c) 2016 University Of Helsinki (The National Library Of Finland)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ **/
+
+(function (root, factory) {
+
+  'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    define(['chai/chai', 'simple-mock', 'loglevel', '../lib/main'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('chai'), require('simple-mock'), require('loglevel'), require('../lib/main'));
+  }
+
+}(this, factory));
+
+function factory(chai, simple, loglevel, initPlugin)
+{
+
+  'use strict';
+
+  var expect = chai.expect;
+
+  describe('main', function() {
+
+    function initMockPlugin(log, buffer)
+    {
+
+      log.methodFactory = function()
+      {
+        return function(msg) {
+          buffer.push(msg);
+        };
+      };
+      
+      log.setLevel(log.getLevel());
+
+    }
+
+    it('Should return the object that was passed in', function() {
+
+      var log = loglevel.getLogger('foobar0');
+
+      expect(initPlugin(log)).to.equal(log);
+
+    });
+
+    it('Should alter #methodFactory', function() {
+
+      var log = loglevel.getLogger('foobar1'),
+      fn_orig = log.methodFactory;
+
+      initPlugin(log);
+
+      expect(log.methodFactory).to.not.equal(fn_orig);
+
+    });
+
+    it('Should call #setLevel', function() {
+
+      var log = loglevel.getLogger('foobar2');
+
+      simple.mock(log, 'setLevel');
+
+      initPlugin(log);
+
+      expect(log.setLevel.called).to.be.true /* jshint -W030 */;
+
+    });
+
+    it('Should not log anything', function() {
+
+      var messages = [],
+      log = loglevel.getLogger('foobar3');
+
+      initMockPlugin(log, messages);
+      initPlugin(log);
+
+      log.error('foobar');
+      
+      expect(messages).to.have.lengthOf(0);
+
+    });
+
+    it('Should log messages after flushing', function() {
+
+      var messages = [],
+      log = loglevel.getLogger('foobar4');
+
+      initMockPlugin(log, messages);
+      initPlugin(log);
+
+      log.error('foobar');
+      
+      expect(messages).to.have.lengthOf(0);
+
+      log.error('foo', 'bar');
+
+      log.flush();
+
+      expect(messages).to.have.lengthOf(2);
+
+    });
+
+    it('Should work both on loglevel object and instances', function() {
+
+      var messages, fn_orig_factory, logger;
+
+      function cleanup()
+      {
+        loglevel.methodFactory = fn_orig_factory;
+        delete loglevel.messageBuffer;
+        delete loglevel.flush;
+      }
+
+      try {
+
+        messages = [];
+        fn_orig_factory = loglevel.methodFactory;
+
+        initMockPlugin(loglevel, messages);
+        initPlugin(loglevel);
+
+        loglevel.error('foo');
+
+        logger = loglevel.getLogger('foobar5');
+
+        logger.error('bar');
+
+        loglevel.flush();
+        expect(messages).to.eql(['foo']);
+                
+        logger.flush();
+        expect(messages).to.eql(['foo', 'bar']);
+        
+        logger.error('foo');
+        loglevel.error('bar');
+
+        logger.flush();
+        expect(messages).to.eql(['foo', 'bar', 'foo']);
+        
+        loglevel.flush();
+        expect(messages).to.eql(['foo', 'bar', 'foo', 'bar']);
+
+        cleanup();
+        
+      } catch (e) {        
+        cleanup();
+        throw e;
+      }
+
+    });
+
+  });
+
+}
